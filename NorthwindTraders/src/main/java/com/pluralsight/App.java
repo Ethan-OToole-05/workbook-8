c1a7ee8efa5f099ebaa1e095fb7b150d05edd546
Ethan.OToole <eotoole005@gmail.com>
2025-06-09 13:59:40 -0400

    Made connection to Northwind db

---
+++ NorthwindTraders/src/main/java/com/pluralsight/App.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.sql.*;
+import javax.sql.DataSource;
+
+public class App {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        //Loading the MySQL Driver.
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection("" +
+                "jdbc:mysql://localhost:3306/northwind",
+                "root",
+                "yearup");
+
+        Statement statement = connection.createStatement();
+
+        String query  = "SELECT * FROM products";
+
+        ResultSet results = statement.executeQuery(query);
+
+        while(results.next()) {
+            String product = results.getString("ProductName");
+            System.out.println(product);
+        }
+        connection.close();
+    }
+
+}
